aeb2c53c64a9262dce061a1065479c94c6c8f4f9
andrewscristhian <andrewsfogo@gmail.com>
2019-12-19 23:45:41 -0300

    Add files via upload

---
+++ RestWithASP-NETCore 08 - Working With Value Objects/RestWithASP-NETCore/Data/Converter/IParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RestWithASP_NETCore.Data.Converter
+{
+    public interface IParser<O, D>
+    {
+        D Parse(O origin);
+        List<D> ParseList(List<O> origin);
+    }
+}
