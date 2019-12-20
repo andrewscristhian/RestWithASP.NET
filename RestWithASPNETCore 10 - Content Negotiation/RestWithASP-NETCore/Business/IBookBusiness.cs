7b0f053e3dac831e08ca2afb533243b3734044df
andrewscristhian <andrewsfogo@gmail.com>
2019-12-19 23:47:40 -0300

    Add files via upload

---
+++ RestWithASPNETCore 10 - Content Negotiation/RestWithASP-NETCore/Business/IBookBusiness.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASP_NETCore.Data.VO;
+using System.Collections.Generic;
+
+namespace RestWithASP_NETCore.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+        BookVO FindById(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO book);
+        void Delete(long id);
+    }
+}
---
+++ RestWithASPNETCore 10 - Content Negotiation/RestWithASP-NETCore/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using RestWithASP_NETCore.Business;
+using RestWithASP_NETCore.Business.Implementations;
+using RestWithASP_NETCore.Model.Context;
+using RestWithASP_NETCore.Repository;
+using RestWithASP_NETCore.Repository.Generic;
+using RestWithASP_NETCore.Repository.Implementations;
+
+namespace RestWithASP_NETCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["MySqlConnection:MySqlConnectionString"];
+            services.AddDbContext<MySQLContext>(options => options.UseMySql(connection));
+
+            //SEE More Details in:  https://blog.jeremylikness.com/5-rest-api-designs-in-dot-net-core-1-29a8527e999chttps://blog.jeremylikness.com/5-rest-api-designs-in-dot-net-core-1-29a8527e999c
+            services.AddMvc(options =>
+                {
+                    options.RespectBrowserAcceptHeader = true;
+                    options.FormatterMappings.SetMediaTypeMappingForFormat("xml", MediaTypeHeaderValue.Parse("text/xml"));
+                    options.FormatterMappings.SetMediaTypeMappingForFormat("json", MediaTypeHeaderValue.Parse("application/json"));
+
+                })
+                .AddXmlSerializerFormatters();
+
+            services.AddApiVersioning(option => option.ReportApiVersions = true);
+
+            //Dependency Injection
+            services.AddScoped<IPersonBusiness, PersonBusiness>();
+            services.AddScoped<IBookBusiness, BookBusiness>();
+
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            //Dependency Injection of GenericRepository
+            services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+        }
+    }
+}
