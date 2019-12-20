7063fe97fd76c24b7abe2c36d51a71c2f89079a5
andrewscristhian <andrewsfogo@gmail.com>
2019-12-19 23:51:57 -0300

    Add files via upload

---
+++ RestWithASPNETCore 18 - Final/RestWithASP-NETCore/Repository/Implementations/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using RestWithASP_NETCore.Model;
+using RestWithASP_NETCore.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASP_NETCore.Repository.Implementations
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly MySQLContext _context;
+
+        public UserRepository(MySQLContext context)
+        {
+            _context = context;
+        }
+
+        public User FindByLogin(string login)
+        {
+            return _context.Users.SingleOrDefault(p => p.Login.Equals(login));
+        }
+    }
+}
